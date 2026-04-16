571a2503850bf5fb76d1dea761b2570e0ee1bfc4
Kyle Molinyawe <kylelorenzo.molinyawe.cics@ust.edu.ph>
2026-04-16 13:39:37 +0800

    added header

--- app/src/main/java/com/example/talipapapp/ui/components/cart/CartScreen.kt
+++ app/src/main/java/com/example/talipapapp/ui/components/cart/CartScreen.kt
@@ -1,31 +1,51 @@
 package com.example.talipapapp.ui.components.cart
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.talipapapp.data.SellerDataSource
+import com.example.talipapapp.ui.components.browse.CategoryBar
+import com.example.talipapapp.ui.components.browse.SellerCard
 
 @Composable
 fun CartScreen() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        // Icon on the screen
-        Icon(
-            imageVector = Icons.Default.ShoppingCart,
-            contentDescription = "cart",
-            tint = Color(0xFF0F9D58)
-        )
-        // Text on the screen
-        Text(text = "Cart", color = Color.Black)
+    Column(modifier = Modifier.fillMaxSize()) {
+
+        // Header
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color(0xFF0F5E3B))
+                .padding(top = 48.dp, start = 16.dp, end = 16.dp, bottom = 16.dp)
+        ) {
+            Text("My Cart",
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+                fontSize = 32.sp)
+            Text("3 items - Est. delivery April 21", color = Color(0xFFE0E0E0))
+
+        }
+
     }
+
 }
